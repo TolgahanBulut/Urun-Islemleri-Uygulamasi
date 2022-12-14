2bfae923fd8694e7dce217758f6e983caa6a5189
Tolgahan <tolga_han_345@hotmail.com>
2022-12-14 23:52:49 +0300

    No commit message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <app-header></app-header>
-    <router-view></router-view>
+    <transition name = "fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <app-footer></app-footer>
   </div>
 </template>
@@ -21,3 +23,17 @@ export default {
   
 }
 </script>
+
+<style>
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity .3s ease-out;
+}
+.fade-leave{}
+.fade-leave-active{
+  transition: opacity .3s ease-out;
+  opacity: 0;
+}
+</style>
